ac12d6ce196fa84c513c1fc565099de537c34b06
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-11-05 18:06:39 +0600

    genesis

---
+++ src/test/java/kz/sapasoft/tests/BasicTestConditions.java
@@ -0,0 +1,10 @@
+package kz.sapasoft.tests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BasicTestConditions {
+    protected static final Logger logger = LogManager.getLogger(BasicTestConditions.class);
+    public String MAIN_URL = "http://dev10.sapasoft.kz/#/int/register";
+
+}
---
+++ src/test/java/kz/sapasoft/tests/LogInTest.java
@@ -0,0 +1,23 @@
+package kz.sapasoft.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class LogInTest extends BasicTestConditions {
+    @Test(groups = "smoke")
+    public void openMain() {
+        open(MAIN_URL);
+        element(Selectors.byXpath("//span[text()='Войти без ЭЦП']")).click();
+        dismiss(); // дабы убрать предупреждение об отсутствии NCA layer
+        element(Selectors.byXpath("//*/label[text()=' Логин ']/../input")).setValue("777777777797_777777777797");
+        element(Selectors.byXpath("//*[@type='password']")).setValue("123456");
+        element(Selectors.byXpath("//*[@type='submit']")).click();
+        logger.info("some info");
+        //Selenide.element(Selectors.byXpath("//*[@type='submit']")).should(Condition.exist);
+
+    }
+}
