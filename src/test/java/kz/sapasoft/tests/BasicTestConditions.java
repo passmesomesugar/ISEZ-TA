3d1b669eb139c2b74317c995b64afad1e56b7415
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-11-08 10:25:04 +0600

    slight refactor, new openHome method introduced for convenience

--- src/test/java/kz/sapasoft/tests/BasicTestConditions.java
+++ src/test/java/kz/sapasoft/tests/BasicTestConditions.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import services.PropertyDataReader;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class BasicTestConditions {
     protected static final Logger logger = LogManager.getLogger(BasicTestConditions.class);
     public String MAIN_URL = "http://dev10.sapasoft.kz/#/int/register";
@@ -11,5 +13,11 @@ public class BasicTestConditions {
 
     public String userName = PropertyDataReader.getProperties(currentEnv).getProperty("user.name");
     public String password = PropertyDataReader.getProperties(currentEnv).getProperty("user.password");
+
+    public void openHome() {
+        open(MAIN_URL);
+        logger.info("Main/home url opened");
+    }
+
 
 }
--- src/test/java/kz/sapasoft/tests/LogInTest.java
+++ src/test/java/kz/sapasoft/tests/LogInTest.java
@@ -10,13 +10,12 @@ import static com.codeborne.selenide.Selenide.*;
 public class LogInTest extends BasicTestConditions {
     @Test(groups = "this")
     public void openMain() {
-        open(MAIN_URL);
+        openHome();
         element(Selectors.byXpath("//span[text()='Войти без ЭЦП']")).click();
         dismiss(); // дабы убрать предупреждение об отсутствии NCA layer
         element(Selectors.byXpath("//*/label[text()=' Логин ']/../input")).setValue(userName);
         element(Selectors.byXpath("//*[@type='password']")).setValue(password);
         element(Selectors.byXpath("//*[@type='submit']")).click();
-        logger.info("some info");
         Selenide.element(Selectors.byId("logout")).should(Condition.exist);
     }
 }
