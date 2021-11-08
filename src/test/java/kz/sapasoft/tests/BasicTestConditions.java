9bf796bb03191fc18bd889cf824e06cf6b0a74e6
Sadvakassov Bagzhan <sadvakasov.bagjan@gmail.com>
2021-11-08 10:17:33 +0600

    ++data reader

--- src/test/java/kz/sapasoft/tests/BasicTestConditions.java
+++ src/test/java/kz/sapasoft/tests/BasicTestConditions.java
@@ -2,8 +2,14 @@ package kz.sapasoft.tests;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import services.PropertyDataReader;
 
 public class BasicTestConditions {
     protected static final Logger logger = LogManager.getLogger(BasicTestConditions.class);
     public String MAIN_URL = "http://dev10.sapasoft.kz/#/int/register";
+    public String currentEnv = System.getProperty("env");
+
+    public String userName = PropertyDataReader.getProperties(currentEnv).getProperty("user.name");
+    public String password = PropertyDataReader.getProperties(currentEnv).getProperty("user.password");
+
 }
---
+++ src/test/java/services/PropertyDataReader.java
@@ -0,0 +1,29 @@
+package services;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyDataReader {
+    private static Properties properties;
+
+    private PropertyDataReader() {
+    }
+
+    public static Properties getProperties(String propertiesFileName) {
+        try {
+            FileReader fileReader =
+                    new FileReader("src/test/resources/" + propertiesFileName + ".properties");
+            properties = new Properties();
+            properties.load(fileReader);
+        } catch (IOException e) {
+            System.out.println();
+        }
+        return properties;
+
+    }
+
+
+}
+
+
